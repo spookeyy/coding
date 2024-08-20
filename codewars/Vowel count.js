8e5e6769aaaa89aadec4bd8433ff7029e6010366
spookeyy <pangasmeshack@gmail.com>
2024-08-20 21:46:17 +0300

    count vowels

---
+++ codewars/Vowel count.js
@@ -0,0 +1,20 @@
+// vowel count
+function getCount(str) {
+    //code goes here
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] === "a" ||
+      str[i] === "e" ||
+      str[i] === "i" ||
+      str[i] === "o" ||
+      str[i] === "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(getCount("abracadabra"))
+// 5
