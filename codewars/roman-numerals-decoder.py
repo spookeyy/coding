8917000b8c92a8c7c39a6bb0f29494be81ccd4ae
spookeyy <pangasmeshack@gmail.com>
2024-08-20 21:45:27 +0300

    roman numerals decoder py

---
+++ codewars/roman-numerals-decoder.py
@@ -0,0 +1,25 @@
+def solution(roman):
+    roman_values = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    total = 0
+    prev_value = 0
+    
+    for char in reversed(roman):
+        current_value = roman_values[char]
+        if current_value >= prev_value:
+            total += current_value
+        else:
+            total -= current_value
+        prev_value = current_value
+    
+    return total
+
+print(solution('MCMXCIV'))
