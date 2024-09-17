76c146cb2975739f1be8181ac54406871d5714c1
spookeyy <pangasmeshack@gmail.com>
2024-09-18 02:59:36 +0300

    find missing letter python

--- codewars/FindMissingLetter.py
+++ codewars/FindMissingLetter.py
@@ -1,14 +1,19 @@
 def find_missing_letter(chars):
     asc_value = [ord(i) for i in chars]
-    print(asc_value)
+    print("asc_value", asc_value)
 
     begin = asc_value[0]
     end = asc_value[-1]
-#     print("begin", begin, "end", end)
+    # print("begin", begin, "end", end)
     
-    expected_sum = sum(range(begin, end+1))
+    expected_sum = sum(range(begin, end+1)) 
     chars_sum = sum(asc_value)
     print("expected", expected_sum, "chars_sum", chars_sum)
+
+    # print(chr(expected_sum - chars_sum))
     
     return chr(expected_sum - chars_sum)
+
+
+find_missing_letter(['a','b','c','d','f'])
 
