091ee84412255fa0c95d772bd142cfe0981fd0f4
spookeyy <pangasmeshack@gmail.com>
2024-09-18 03:11:44 +0300

    adding time complexity comments

--- codewars/FindMissingNumber.py
+++ codewars/FindMissingNumber.py
@@ -1,6 +1,6 @@
 def missing_no(nums):
     
-    expected_sum = sum(range(0, 101))
-    given_sum = sum(nums)
+    expected_sum = sum(range(0, 101)) # time complexity O(n) -> n = 101
+    given_sum = sum(nums)             # time complexity O(n) -> n = len(nums)
     
-    return expected_sum - given_sum
+    return expected_sum - given_sum   # time complexity O(1)  --- TOTAL TIME COMPLEXITY: O(n)
