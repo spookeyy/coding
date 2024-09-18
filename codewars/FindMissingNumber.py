b2a1b76dcee273e168a90d55cd114f5cb977db08
spookeyy <pangasmeshack@gmail.com>
2024-09-18 03:06:05 +0300

    find missing number python

--- codewars/FindMissingNumber.py
+++ codewars/FindMissingNumber.py
@@ -1,2 +1,6 @@
 def missing_no(nums):
-    pass
+    
+    expected_sum = sum(range(0, 101))
+    given_sum = sum(nums)
+    
+    return expected_sum - given_sum
---
+++ leetcode/sumoftwointegers.js
@@ -0,0 +1,7 @@
+// Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+function getSum(a, b){
+    
+}
+
+console.log(getSum(1, 2))
