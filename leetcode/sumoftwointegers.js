895b4e3b2bcd9e0fabfab5bcb70a3bf73bb90012
spookeyy <pangasmeshack@gmail.com>
2024-09-18 04:36:41 +0300

    sum of two integers without using "+"

--- leetcode/sumoftwointegers.js
+++ leetcode/sumoftwointegers.js
@@ -1,7 +1,18 @@
 // Given two integers a and b, return the sum of the two integers without using the operators + and -.
 
 function getSum(a, b){
-    
+
+    while (b != 0){
+        let sum = 0;
+        let carry = 0;
+
+        sum = a ^ b
+        carry = (a & b) << 1
+        a = sum
+        b = carry
+    }
+    return a
 }
 
-console.log(getSum(1, 2))
+console.log(getSum(5, 0)) // 5 but am getting 0 instead of 5
+console.log(getSum(5, 3)) // 8 correct
