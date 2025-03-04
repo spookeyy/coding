3c5ab373bfa0f7fb58c01f06318b5aa832fb8796
spookeyy <pangasmeshack@gmail.com>
2025-03-04 12:12:19 +0300

    student scores

---
+++ zet/scores.py
@@ -0,0 +1,12 @@
+# Create a Python dictionary storing the names of three students and their corresponding scores.
+# Retrieve and print the score of one student.
+
+student_scores = {
+    "James": 60,
+    "Janet": 76,
+    "McDonald": 82
+}
+
+print(student_scores["James"])
+print(student_scores["Janet"])
+print(student_scores["McDonald"])
