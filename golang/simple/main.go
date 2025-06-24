f274a76dc03cf68eb07a6c5f2402fed113575bdd
spookeyy <pangasmeshack@gmail.com>
2025-06-24 12:23:25 +0300

    maths in go

--- golang/simple/main.go
+++ golang/simple/main.go
@@ -16,4 +16,25 @@ func main(){
 	// go func() {
 	// 	fmt.Println("hello world")
 	// }()
+
+	fmt.Println(add(1, 2))
+	fmt.Println(sub(1, 2))
+	fmt.Println(mul(1, 2))
+	fmt.Println(div(1, 2))
+}
+
+func add(a int, b int) int {
+	return a + b
+}
+
+func sub(a int, b int) int {
+	return a - b
+}
+
+func mul(a int, b int) int {
+	return a * b
+}
+
+func div(a int, b int) int {
+	return a / b
 }
